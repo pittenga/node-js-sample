4f3669fb1166c8262ef52ea8e3088fab8c267212
Aaron Pittenger <aaron.pittenger@ge.com>
2017-03-27 11:20:00 -0400

    Updated to fake out a request and log the response to the console

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var request = require('request');
+//var swal = require('sweetalert2');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -11,3 +13,21 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+var requestLoop = setInterval(function(){
+  request({
+      url: "http://www.google.com",
+      method: "GET",
+      timeout: 10000,
+      followRedirect: true,
+      maxRedirects: 10
+  },function(error, response, body){
+      if(!error && response.statusCode == 200){
+          //window.swal('Good response!');
+          console.log("good response!")
+      }else{
+          //window.swal('bad response :( !');
+          console.log("bad response!")
+      }
+  });
+}, 5000);
