41189e192e297ffe7e4f1815fbc7347931ca33a9
Aaron Pittenger <aaron.pittenger@ge.com>
2017-03-29 13:56:33 -0400

    Got style sheets working and a chart up and running

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var request = require('request');
-//var swal = require('sweetalert2');
+const co = require('co');
+const generate = require('node-chartist');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  co(function * () {
+
+  // options object
+  const options = {width: 400, height: 200};
+  const data = {
+    labels: ['a','b','c','d','e'],
+    series: [
+      [1, 2, 3, 4, 5],
+      [3, 4, 5, 6, 7]
+    ]
+  };
+  const bar = yield generate('bar', options, data); //=> chart HTML
+
+
+  // options function
+  const options2 = (Chartist) => ({width: 400, height: 200, axisY: { type: Chartist.FixedScaleAxis } });
+  const data2 = {
+    labels: ['a','b','c','d','e'],
+    series: [
+      [1, 2, 3, 4, 5],
+      [3, 4, 5, 6, 7]
+    ]
+  };
+  const bar2 = yield generate('bar', options2, data2); //=> chart HTML
+  response.send('<head><link rel="stylesheet" href="main.css"> Hello World!</head>' + '<div class="chartHtml">' + bar +  '</div>')
+  });
+
+
+
 })
 
 app.listen(app.get('port'), function() {
@@ -23,10 +53,8 @@ var requestLoop = setInterval(function(){
       maxRedirects: 10
   },function(error, response, body){
       if(!error && response.statusCode == 200){
-          //window.swal('Good response!');
           console.log("good response!")
       }else{
-          //window.swal('bad response :( !');
           console.log("bad response!")
       }
   });
